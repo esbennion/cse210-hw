b31a7db4ce91328f5651ee7f369038fb9a6fc01e
esbennion <ebennion1@byupathway.edu>
2026-03-07 17:09:09 -0600

    finished Exercise 3

--- week01/Exercise3/Program.cs
+++ week01/Exercise3/Program.cs
@@ -4,6 +4,35 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise3 Project.");
+        Console.WriteLine("Hello World! This is the Exercise 3 Project.");
+        //Console.Write("What is the magic number? ");
+        //int magicNumber = int.Parse(Console.ReadLine());
+
+        Random randomGenerator = new Random();
+        int magicNumber = randomGenerator.Next(1,101);
+
+        int guess = -1;
+        
+        while(guess != magicNumber)
+        {
+            Console.Write("Try and pick what number I am thinking. What is your guess? ");
+            guess = int.Parse(Console.ReadLine());
+
+            if (magicNumber > guess)
+            {
+                Console.WriteLine("Go higher!");
+            }
+
+            else if (magicNumber < guess)
+            {
+                Console.WriteLine("Try lower!");
+            }
+            else
+            {
+                Console.WriteLine("Correct, You guessed right!");
+            }
+
+        }
+
     }
 }
