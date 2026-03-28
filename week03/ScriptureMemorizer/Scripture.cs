544345345a86ab25cb4bd51decba5f5e606e4cdc
esbennion <ebennion1@byupathway.edu>
2026-03-27 21:45:12 -0500

    week3 project scriputre memorizer

---
+++ week03/ScriptureMemorizer/Scripture.cs
@@ -0,0 +1,53 @@
+using System;
+using System.Collections.Generic;
+
+public class Scripture
+{
+    private Reference _reference;
+    private List<Word> _words;
+
+    public Scripture(Reference reference, string text)
+    {
+        _reference = reference;
+        _words = new List<Word>();
+
+        string [] parts = text.Split(' ');
+        foreach (string part in parts)
+        {
+            _words.Add(new Word(part));
+        }
+    }
+
+    public void HideRandomWords(int numberToHide)
+    {
+        Random random = new Random();
+        for (int i = 0; i < numberToHide; i++)
+        {
+            int index = random.Next(_words.Count);
+            _words[index].Hide();
+        }
+    }
+
+    public bool IsCompletelyHidden()
+    {
+        foreach (Word word in _words)
+        {
+            if (!word.IsHidden())
+            {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    public string GetDisplayText()
+    {
+        List<string> displayWords = new List<string>();
+        foreach(Word word in _words)
+        {
+            displayWords.Add(word.GetDisplayText());
+        }
+        string text = string.Join(" ", displayWords);
+        return $"{_reference.GetDisplayText()} {text}";
+    }
+}
