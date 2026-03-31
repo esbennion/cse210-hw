92342ba76d0f53034e059bdcba293927aea1bc6e
esbennion <ebennion1@byupathway.edu>
2026-03-31 13:14:02 -0500

    Onlline Ordering Program finished

---
+++ week04/OnlineOrdering/Order.cs
@@ -0,0 +1,44 @@
+public class Order
+{
+    private Customer _customer;
+    private List<Product>_products = new List<Product>();
+    public Order(Customer customer)
+    {
+        _customer = customer;
+    }
+
+    public void AddProduct(Product product)
+    {
+        _products.Add(product);
+    }
+
+    public double GetTotalCost()
+    {
+        double total = 0;
+
+        foreach (Product p in _products)
+        {
+            total += p.GetTotalCost();
+        }
+
+        double shipping = _customer.LivesInUSA() ? 5 : 35;
+        return total + shipping;
+    }
+    public string GetPackingLabel()
+    {
+        string label = "Packing Label: \n";
+
+        foreach (Product p in _products)
+        {
+            label += p.GetPackingInfo() + "\n";
+        }
+        return label;
+    }
+
+    public string GetShippingLabel()
+    {
+        return $"Shipping Label:\n{_customer.GetName()}\n{_customer.GetAddressString()}";
+    }
+
+
+}
--- week04/OnlineOrdering/Program.cs
+++ week04/OnlineOrdering/Program.cs
@@ -1,9 +1,27 @@
-using System;
-
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the OnlineOrdering Project.");
+        Address a1 = new Address("111 Mom Lane", "Des Moines", "IA", "USA");
+        Customer c1 = new Customer("Emily Bennion", a1);
+
+        Order order1 = new Order(c1);
+        order1.AddProduct(new Product("Water Bottle", "A1A", 15.00, 1));
+        order1.AddProduct(new Product("Straw Brush", "B1B", 5.50, 1));
+
+        Console.WriteLine(order1.GetPackingLabel());
+        Console.WriteLine(order1.GetShippingLabel());
+        Console.WriteLine($"Total Price: {order1.GetTotalCost()}\n");
+
+        Address a2 = new Address("222 Dad Blvd", "North Liberty", "IA", "USA");
+        Customer c2 = new Customer("Paul Walker", a2);
+
+        Order order2 = new Order(c2);
+        order2.AddProduct(new Product("Tennis Racket","A2AB", 25.75, 2));
+        order2.AddProduct(new Product("Tennis Balls", "B2BC", 9.00, 3));
+
+        Console.WriteLine(order2.GetPackingLabel());
+        Console.WriteLine(order2.GetShippingLabel());
+        Console.WriteLine($"Total Price: ${order2.GetTotalCost()}\n");
     }
 }
