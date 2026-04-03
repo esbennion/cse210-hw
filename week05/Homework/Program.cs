b4d5072cf848b6b2167b733105b42582b134b1bd
esbennion <ebennion1@byupathway.edu>
2026-04-02 20:57:53 -0500

    Completed Learning Activity Inheritance

--- week05/Homework/Program.cs
+++ week05/Homework/Program.cs
@@ -4,6 +4,17 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Homework Project.");
+        Assignment a1 = new Assignment("Emily Bennion", "Addition");
+
+        string summary = a1.GetSummary();
+        Console.WriteLine(a1.GetSummary());
+
+        MathAssignment a2 = new MathAssignment("Natasha Smith", "Multiplication", "2.2", "4-14");
+        Console.WriteLine(a2.GetSummary());
+        Console.WriteLine(a2.GetHomeworkList());
+
+        WritingAssignment a3 = new WritingAssignment("Bob Bobby", "American History", "Events Leading to the Civil War");
+        Console.WriteLine(a3.GetSummary());
+        Console.WriteLine(a3.GetWrititingInformation());
     }
 }
